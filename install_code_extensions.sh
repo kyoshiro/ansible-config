e0d7dc255cde46ce7c8b6b2f4013741faf928ff1
kyoshiro <108440+kyoshiro@users.noreply.github.com>
2019-11-30 20:37:02 +0100

    added more useful stuff

--- install_code_extensions.sh
+++ install_code_extensions.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-declare -a StringArray=("jpogran.puppet-vscode" "ms-kubernetes-tools.vscode-kubernetes-tools" "ms-python.python" "redhat.vscode-yaml" "vscoss.vscode-ansible" "yzhang.markdown-all-in-one")
+declare -a StringArray=("jpogran.puppet-vscode"
+                        "ms-kubernetes-tools.vscode-kubernetes-tools"
+                        "ms-python.python"
+                        "redhat.vscode-yaml"
+                        "vscoss.vscode-ansible"
+                        "yzhang.markdown-all-in-one"
+                        "waderyan.gitblame"
+                        "eamodio.gitlens"
+                        "remisa.shellman"}
 
 for val in ${StringArray[@]}; do
    sudo -H -u kyoshiro bash -c "code --install-extension $val --force"
