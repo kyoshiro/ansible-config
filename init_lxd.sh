2e7376ee1a574d2b7298dbbfe736a5fb0c9e0d2a
kyoshiro <108440+kyoshiro@users.noreply.github.com>
2020-01-16 21:25:12 +0100

    changed lxd service check and initialization parameter

--- init_lxd.sh
+++ init_lxd.sh
@@ -2,15 +2,18 @@
 
 # Preflight check if lxd is already up and running
 if [ -e /var/run/snapd/lock/lxd.lock ]; then
-	echo "LXD already configured. Skipping."
-else
-  # Run the lxd init
+	echo "LXD running. Stopping service."
+	systemctl stop snap.lxd.socket.service
+	systemctl stop snap.lxd.daemon.service
+fi
+# Run the lxd init
 cat <<EOF | /snap/bin/lxd init --preseed
 config: {}
 networks:
 - config:
-    ipv4.address: auto
-    ipv6.address: auto
+  ipv4.address: 10.177.2.1/24
+  ipv4.nat: "true"
+  ipv6.address: none
   description: ""
   managed: false
   name: lxdbr0
@@ -37,4 +40,6 @@ profiles:
   name: default
 cluster: null
 EOF
-fi
+touch /root/.lxd_init
+systemctl start snap.lxd.daemon.service
+systemctl start snap.lxd.socket.service
