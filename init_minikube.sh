3a8b581aefd9a99815f4d110b869424116cc79a0
kyoshiro <108440+kyoshiro@users.noreply.github.com>
2020-02-21 22:20:52 +0100

    fixed init minikube using yaml file

--- init_minikube.sh
+++ init_minikube.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
 lxc profile create minikube
 
-cat << EOF | lxc profile edit minicube
-name: minikube
-config:
-  linux.kernel_modules: ip_tables,ip6_tables,netlink_diag,nf_nat,overlay
-  raw.lxc: |
-    lxc.apparmor_profile=unconfined
-    lxc.mount.auto=proc:rw sys:rw
-    lxc.cap.drop=
-  security.nesting: "true"
-  security.privileged: "true"
-description: Profile supporting minikube in containers
-devices:
-  aadisable:
-    path: /sys/module/apparmor/parameters/enabled
-    source: /dev/null
-    type: disk
-EOF
+cat minikube.yaml | lxc profile edit minicube
 
-lxc launch ubuntu:18.04 minikube
+lxc launch ubuntu:19.10 minikube
 lxc profile apply minikube default,minikube
 
